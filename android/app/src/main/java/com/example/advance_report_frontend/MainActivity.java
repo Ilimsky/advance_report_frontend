323746a05d86e569aed1a5ec724ccb33f652128d
Ilimsky <ilim.sarykbaev@gmail.com>
2025-10-04 22:13:08 +0600

    04.10.2025 22:13

--- android/app/src/main/java/com/example/advance_report_frontend/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.advance_report_frontend;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "43338579a56b579f7cf04d7519e31f90
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8db645d4ec9881b9d4e3c36ff57800b2",
+"flutter_bootstrap.js": "3dc1991e7d3ee0b3faf2fbd5180d15e7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0ee61757f28fd5db27c7637d5b0d04ac",
 "/": "0ee61757f28fd5db27c7637d5b0d04ac",
-"main.dart.js": "767aa4e187d9cb2a3a588a07503e1ac4",
+"main.dart.js": "4489cc4b301de659cf75b193a773e075",
 "manifest.json": "0faa05fd017247fb9c5b94cb6dfc0d62",
 "version.json": "8d3f11cf366c48f299835c6969b626da"};
 // The application shell files that are downloaded before a service worker can
